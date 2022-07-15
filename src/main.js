ebca953252f915cb4a45f2d1cb3163b75ca12763
培培 <614963845@qq.com>
2022-07-15 18:56:09 +0800

    feat(element): 国际化

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import store from './store'
 import './styles/reseet.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-CN'
 
 const app = createApp(App)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn
+})
 app.use(store).use(router).mount('#app')
