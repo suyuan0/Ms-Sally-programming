457a3660dfe75aef5aada09ca2a128cafb27bbce
培培 <614963845@qq.com>
2022-07-20 10:50:44 +0800

    feat(cateGoryList): 分类管理(添加 修改状态 编辑 删除 推荐商品渲染 关联没写)

--- src/api/category.js
+++ src/api/category.js
@@ -12,6 +12,30 @@ export const getCategoryListAPI = (data) => {
     data
   })
 }
+/**
+ * 添加分类
+ * @param data
+ * @returns {AxiosPromise}
+ */
+export const addCategoryAPI = (data) => {
+  return request({
+    url: '/category',
+    method: 'POST',
+    data
+  })
+}
+/**
+ * 修改
+ * @param data
+ * @returns {AxiosPromise}
+ */
+export const editCategoryAPI = (data) => {
+  return request({
+    url: `/category/${data.id}`,
+    method: 'POST',
+    data
+  })
+}
 
 /**
  * 推荐商品
@@ -23,5 +47,28 @@ export const appCategortItemAPI = (data) => {
     url: '/app_category_item/list',
     method: 'GET',
     data
+  })
+}
+/**
+ * 修改状态
+ * @param data
+ * @returns {AxiosPromise}
+ */
+export const updateStatusAPI = (id, data) => {
+  return request({
+    url: `/category/${id}/update_status`,
+    method: 'POST',
+    data
+  })
+}
+/**
+ * 删除分类
+ * @param id
+ * @returns {AxiosPromise}
+ */
+export const deleteCategoryAPI = (id) => {
+  return request({
+    url: `/category/${id}/delete`,
+    method: 'POST'
   })
 }
--- src/views/shop_category_list/index.vue
+++ src/views/shop_category_list/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card shadow='never'>
     <div class='search'>
-      <el-button size='small' type='primary'>新增</el-button>
+      <el-button size='small' type='primary' @click='addDrawerShow=true'>新增</el-button>
       <el-tooltip content='刷新数据' placement='top'>
         <el-button icon='Refresh' text @click='getCategoryList'></el-button>
       </el-tooltip>
@@ -13,15 +13,20 @@
           <span>{{ node.label }}</span>
           <span>
             <el-button :loading='data.loading' link type='primary' @click='recommendGoods(data)'>推荐商品</el-button>
-            <el-switch v-model='data.status' :active-value='1' :inactive-value='0' style='margin: 0 10px'></el-switch>
-            <el-button link type='primary'>修改</el-button>
-            <el-button link type='primary'>删除</el-button>
+            <el-switch v-model='data.status' :active-value='1' :inactive-value='0' style='margin: 0 10px'
+                       @change='(status)=>handleChangeStatus(status,data)'></el-switch>
+            <el-button link type='primary' @click='handleEditCatrgory(data)'>修改</el-button>
+            <el-popconfirm title='是否要删除该记录?' @confirm='handleDeleteCategory(data.id)'>
+              <template #reference>
+                <el-button link type='primary'>删除</el-button>
+              </template>
+            </el-popconfirm>
           </span>
         </span>
       </template>
     </el-tree>
   </el-card>
-  <!--  抽屉-->
+  <!--  推荐抽屉-->
   <el-drawer v-model='drawerShow' size='40%'>
     <template #header>
       <span>推荐商品</span>
@@ -47,19 +52,66 @@
       </div>
     </template>
   </el-drawer>
+  <!--  新增--添加-抽屉-->
+  <el-drawer v-model='addDrawerShow' size='40%'>
+    <template #header>
+      <span>{{ drawerTitle }}</span>
+    </template>
+    <el-form ref='categoryFormRef' :model='categoryModel' :rules='rules' label-width='80px'>
+      <el-form-item label='分类名称' prop='name'>
+        <el-input v-model='categoryModel.name' placeholder='分类名称'></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class='footer'>
+        <el-button type='primary' @click='handleSubmit'>提交</el-button>
+        <el-button @click='handleResetForm'>取消</el-button>
+      </div>
+    </template>
+  </el-drawer>
 </template>
 
 <script setup>
-import { getCategoryListAPI, appCategortItemAPI } from '@/api/category'
-import { ref } from 'vue'
+import {
+  getCategoryListAPI,
+  appCategortItemAPI,
+  addCategoryAPI,
+  editCategoryAPI,
+  updateStatusAPI,
+  deleteCategoryAPI
+} from '@/api/category'
+import { computed, reactive, ref } from 'vue'
 import ATable from '@/components/Table/a-table'
 import column from './column'
+import { Notification } from '@/utils/Notification'
+// 新增或添加抽屉显示隐藏
+const addDrawerShow = ref(false)
 // 抽屉显示隐藏
 const drawerShow = ref(false)
+// 表单ref
+const categoryFormRef = ref(null)
+// 添加--修改表单模型
+const categoryModel = reactive({
+  name: ''
+})
+// 表单验证
+const rules = {
+  name: [
+    {
+      required: true,
+      message: '请输入分类名称',
+      trigger: 'blur'
+    }
+  ]
+}
 // tree配置项
 const treeProps = {
   label: 'name'
 }
+// 标题
+const drawerTitle = computed(() => {
+  return categoryModel.id ? '修改' : '新增'
+})
 // 推荐商品列表
 const recommendGoodsList = ref([])
 // 分类列表
@@ -89,6 +141,50 @@ const recommendGoods = async (data) => {
     console.log(e)
   }
   data.loading = false
+}
+// 添加数据--修改数据
+const handleSubmit = async () => {
+  try {
+    await categoryFormRef.value.validate()
+    await (categoryModel.id ? editCategoryAPI : addCategoryAPI)(categoryModel)
+    addDrawerShow.value = false
+    await getCategoryList()
+    Notification(categoryModel.id ? '修改成功' : '添加成功', '', 'success')
+  } catch (e) {
+    console.log(e)
+  }
+}
+// 关闭抽屉，重置表单
+const handleResetForm = () => {
+  categoryFormRef.value.resetFields()
+  addDrawerShow.value = false
+}
+// 修改
+const handleEditCatrgory = (data) => {
+  for (const key in data) {
+    categoryModel[key] = data[key]
+  }
+  addDrawerShow.value = true
+}
+// 修改状态
+const handleChangeStatus = async (status, data) => {
+  try {
+    await updateStatusAPI(data.id, { status })
+    await getCategoryList()
+    Notification('修改状态成功', '', 'success')
+  } catch (e) {
+    console.log(e)
+  }
+}
+// 删除
+const handleDeleteCategory = async (id) => {
+  try {
+    await deleteCategoryAPI(id)
+    await getCategoryList()
+    Notification('删除成功', '', 'success')
+  } catch (e) {
+    console.log(e)
+  }
 }
 </script>
 
